0035ab5205c4a8267b81657f2d2cdeddaaac5f55
shibli21 <syedshiblimahmud@gmail.com>
2022-01-30 22:33:39 +0600

    banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,34 +5,39 @@ interface Props {}
 
 export const Banner = (props: Props) => {
   return (
-    <div className="">
-      <div className="py-12 text-start lg:py-16">
-        <h2 className="inline-block px-4 py-2 mt-20 text-6xl font-extrabold  transform -skew-x-12 bg-orange-500  sm:text-8xl">
+    <div className="py-0 md:py-4 lg:py-10 grid grid-cols-1  md:grid-cols-1 lg:grid-cols-2 items-end">
+      <div className="">
+        <h2 className="inline-block px-4 py-2 mt-8  text-6xl font-extrabold  transform -skew-x-12 bg-orange-500  sm:text-8xl">
           <span className="uppercase">Trending</span>
         </h2>
         <p className="mt-2 text-lg italic font-semibold  sm:text-3xl">Discover the best games in the world</p>
-
-        <div className="pt-10">
-          <Button
-            icon={
-              <svg
-                className="w-5 h-5 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            }
-            isLoading={true}
-          >
-            Find Near Your
-          </Button>
-        </div>
+      </div>
+      <div className="flex gap-3 my-5 h-[40px] justify-end ">
+        <input
+          type="text"
+          id="search"
+          className=" bg-gray-50 border-4 lg:max-w-[300px]  border-orange-500 text-gray-900 text-sm  block w-full p-2.5 focus:outline-none"
+          required
+        />
+        <Button
+          icon={
+            <svg
+              className="w-5 h-5 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          }
+          isLoading={true}
+        >
+          Search
+        </Button>
       </div>
     </div>
   );
--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({ children, icon, isLoading }: Props) => {
   return (
     <button
       type="button"
-      className="text-white group font-medium text-sm px-5 py-2.5 text-center inline-flex justify-center items-center bg-orange-500 rounded-lg shadow-md hover:bg-orange-600   focus:outline-none"
+      className="text-white group font-medium text-sm px-5 py-2.5 text-center inline-flex justify-center items-center bg-orange-500  shadow-md hover:bg-orange-600   focus:outline-none"
     >
       {!isLoading ? (
         <div className="w-5 h-5 border-2 border-top-color:transparent border-white border-dashed rounded-full animate-spin"></div>
