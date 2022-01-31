ac53b5b5799e86ad9679fb3f723a6c35aa5adc8d
shibli21 <syedshiblimahmud@gmail.com>
2022-02-01 00:27:17 +0600

    on route change loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import { Router } from "next/router";
 import Footer from "../components/Footer";
 import { GamesProvider } from "../store/games-context";
 import "../styles/globals.css";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
