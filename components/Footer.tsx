74585f00b5f3d3e3cc745ce6aacbcc941c041991
shibli21 <syedshiblimahmud@gmail.com>
2022-02-01 12:18:18 +0600

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 interface Props {}
 
 const Footer = (props: Props) => {
   return (
-    <footer className="mt-20 py-10 mx-auto shadow-orange-400 shadow">
-      <p className="text-center font-mono"> ©{new Date().getFullYear()} Shibli. All rights reserved.</p>
+    <footer className="py-10 mx-auto mt-20 shadow shadow-orange-400">
+      <p className="font-mono text-center"> ©{new Date().getFullYear()} Shibli. All rights reserved.</p>
+      <div className="flex justify-center gap-3 py-2">
+        <a href="https://github.com/shibli21" rel="noreferrer" target="_blank" aria-label="Github">
+          <FaGithub size="20px" className="cursor-pointer hover:text-orange-500" />
+        </a>
+        <a href="https://twitter.com/shibli21" rel="noreferrer" target="_blank" aria-label="Twitter">
+          <FaTwitter size="20px" className="cursor-pointer hover:text-orange-500" />
+        </a>
+        <a href="https://twitter.com/shibli21" rel="noreferrer" target="_blank" aria-label="Instagram">
+          <FaInstagram size="20px" className="cursor-pointer hover:text-orange-500" />
+        </a>
+        <a href="https://www.linkedin.com/in/shibli21/" rel="noreferrer" target="_blank" aria-label="Linkedin">
+          <FaLinkedin size="20px" className="cursor-pointer hover:text-orange-500" />
+        </a>
+      </div>
     </footer>
   );
 };
